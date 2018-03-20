81688d0b50f5dfb5d3651092deebd70806d55386
Robert Goon <rob.goon@gmail.com>
2018-03-20 12:32:33 -0400

    fixed expected array

--- twoSum.spec.js
+++ twoSum.spec.js
@@ -3,10 +3,10 @@ const { expect } = require('chai');
 
 describe('twoSum', () => {
   it('twoSum returns empty array', () => {
-    expect(twoSum([1, 2, 3], 6)).to.equal([]);
+    expect(twoSum([1, 2, 3], 6)).to.deep.equal([]);
   });
 
   it('twoSum returns proper array', () => {
-    expect(twoSum([1, 2, 3], 5)).to.equal([1, 2]);
+    expect(twoSum([1, 2, 3], 5)).to.eql([1, 2]);
   });
 })
